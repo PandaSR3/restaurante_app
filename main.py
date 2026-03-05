c14898784d5839f2b3d39267d50490a1418c1a99
Caleb <ganadorxd84265@gmail.com>
2026-03-04 20:02:58 -0500

    Fix SSL Render

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ import os
 ADMIN_PASSWORD = "1234"
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"},
+    pool_pre_ping=True
+)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
